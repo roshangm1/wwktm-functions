28af9af68298c6963bdd9df344241d569ffa4631
Aawaz Gyawali <awazgyawali@gmail.com>
2019-08-22 00:08:23 +0545

    Added upvote function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,33 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+exports.upvoteAQuestion = functions.https.onCall(async (data, context) => {
+  var questionId = data.questionId;
+  var uid = context.auth!.uid;
+  var question = (await admin
+    .firestore()
+    .collection("questions")
+    .doc(questionId)
+    .get()).data();
+
+  if (question.voters.includes(uid)) {
+    question.voters = arrayRemove(question.voters, uid);
+  } else {
+    question.voters.push(uid);
+  }
+  question.upvotes = question.voters.length;
+
+  await admin
+    .firestore()
+    .collection("questions")
+    .doc(questionId)
+    .update(question);
+});
+
+function arrayRemove(arr: [], value: string) {
+  return arr.filter(function(ele) {
+    return ele != value;
+  });
+}
