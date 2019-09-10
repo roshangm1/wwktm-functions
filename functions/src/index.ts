0caa2382fc5c7b92876dd165cfbac4404b184284
Aawaz Gyawali <awazgyawali@gmail.com>
2019-09-10 11:06:05 +0545

    Image compressed

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,11 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+const gcs = require("@google-cloud/storage")();
+const spawn = require("child-process-promise").spawn;
+const mkdirp = require("mkdirp-promise");
+const path = require("path");
+const os = require("os");
+const fs = require("fs");
 
 admin.initializeApp();
 
@@ -59,3 +65,57 @@ exports.onQuestionEdited = functions.firestore
       .doc(context.params.questionId)
       .update({ upvoteCount: count });
   });
+
+exports.compressNewImage = functions.storage.object().onFinalize(data => {
+  // File and directory paths.
+  const filePath = data.name;
+  const tempLocalFile = path.join(os.tmpdir(), filePath);
+  const tempLocalDir = path.dirname(tempLocalFile);
+
+  // Exit if this is triggered on a file that is not an image.
+  if (!data.contentType!.startsWith("image/")) {
+    console.log("This is not an image.");
+    return null;
+  }
+
+  if (data.metadata && data.metadata!.optimized) {
+    console.log("This is not an image.");
+    return null;
+  }
+
+  // Cloud Storage files.
+  const bucket = gcs.bucket(data.bucket);
+  const file = bucket.file(filePath);
+  return mkdirp(tempLocalDir)
+    .then(() => file.download({ destination: tempLocalFile }))
+    .then(() => {
+      console.log("The file has been downloaded to", tempLocalFile);
+      return spawn("convert", [
+        tempLocalFile,
+        "-strip",
+        "-interlace",
+        "Plane",
+        "-quality",
+        "90",
+        tempLocalFile
+      ]);
+    })
+    .then(() => {
+      console.log("Optimized image created at", tempLocalFile);
+      // Uploading the Optimized image.
+      return bucket.upload(tempLocalFile, {
+        destination: file,
+        metadata: {
+          metadata: {
+            optimized: true
+          }
+        }
+      });
+    })
+    .then(() => {
+      console.log("Optimized image uploaded to Storage at", file);
+      // Once the image has been uploaded delete the local files to free up disk space.
+      fs.unlinkSync(tempLocalFile);
+      return null;
+    });
+});
